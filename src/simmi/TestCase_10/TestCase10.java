a31cc4738c84deac11f942dd07dc0069e1e8f361
Simmi <simmisingh217@gmail.com>
2021-06-18 17:34:58 +0530

    Simmi_TestCases_7to12

---
+++ src/simmi/TestCase_10/TestCase10.java
@@ -0,0 +1,48 @@
+package simmi.TestCase_10;
+
+import java.util.HashMap;
+
+/*Test case : 10
+
+Name -> "Maulik", print lastName and UserName*/
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class TestCase10 {
+
+	static HashMap<String, String> getEmployeeDetails(String name, WebDriver driver) {
+		String lastName = driver
+				.findElement(By.xpath("//table[@id='table1']/tbody//td[text()='Maulik']//following-sibling::td[1]"))
+				.getText();
+		String userName = driver
+				.findElement(By.xpath("//table[@id='table1']/tbody//td[text()='Maulik']//following-sibling::td[2]"))
+				.getText();
+
+		HashMap<String, String> empDetailsMap = new HashMap<String, String>();
+		empDetailsMap.put("Last Name", lastName);
+		empDetailsMap.put("Username", userName);
+
+		return empDetailsMap;
+	}
+
+	public static void main(String[] args) {
+
+		System.setProperty("webdriver.chrome.driver", "./resources/chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+		driver.get("http://automationbykrishna.com");
+		driver.manage().window().maximize();
+
+		driver.manage().timeouts().implicitlyWait(3000, TimeUnit.MILLISECONDS);
+		driver.findElement(By.xpath("//a[@id='demotable']")).click();
+
+		String name = driver.findElement(By.xpath("//table[@id='table1']/tbody/tr[1]/td[2]")).getText();
+
+		HashMap<String, String> getDetails = getEmployeeDetails(name, driver);
+		System.out.println("LastName and UserName of Maulik is " + getDetails);
+		driver.close();
+	}
+}
