c7686a0297c2b2f3fcb0e9165007690d20d12693
VaibhavTati <vaibhavtati@gmail.com>
2021-06-23 22:25:55 +0530

    Practice_1

---
+++ src/vaibhav/base/PredefinedActions.java
@@ -0,0 +1,15 @@
+package vaibhav.base;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class PredefinedActions {
+	
+	public static WebDriver start(String url) {
+		System.setProperty("webdriver.chrome.driver", ".\\resources\\chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+		driver.get(url);
+		return driver;
+	}
+
+}
