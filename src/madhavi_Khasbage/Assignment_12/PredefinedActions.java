a2dc77ae2c088dfe5ae120f3879708664436cc20
Madhavi Khasbage <madhavikhasbage@yahoo.in>
2021-06-25 23:50:31 +0530

    madhaviK_Assignment_12

---
+++ src/madhavi_Khasbage/Assignment_12/PredefinedActions.java
@@ -0,0 +1,13 @@
+package madhavi_Khasbage.Assignment_12;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class PredefinedActions {
+	public static WebDriver setUp(String url) {
+		System.getProperty("wedriver.chrome.driver", "chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+		driver.get(url);
+		return driver;
+	}
+}
